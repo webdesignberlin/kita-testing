63d2e61d5044912b5afdbbceb724d6e05b68cba3
Michael Raguse <webdesignberlin@users.noreply.github.com>
2022-01-05 19:37:09 +0100

    pwa config

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,33 @@ import { VitePWA } from 'vite-plugin-pwa';
 export default defineConfig({
   plugins: [
     vue(),
-    VitePWA(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Kita-Tests',
+        short_name: 'Kita-Tests',
+        description: 'Prevent Papers',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          }
+        ]
+      }
+    }),
   ]
 });
